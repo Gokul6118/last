be273ac90bded3f5a781da20a298449ae0f10554
Gokul-Webzenith <gokul@webzenith.tech>
2026-02-02 20:03:21 +0530

    ff

--- apps/hono-backend/api/index.ts
+++ apps/hono-backend/api/index.ts
@@ -2,7 +2,7 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import { logger } from 'hono/logger'
-import { serve } from '@hono/node-server'
+
 import  {handle} from 'hono/vercel'
 
 type Todo = {
